3963886f7b92d0ee1fd385d3fdc84ef79f02def4
traversgrayson <travers.parsonsgrayson@gmail.com>
2018-05-06 17:57:59 -0400

    More stuff

--- hockeyStatsnew.R
+++ hockeyStatsnew.R
@@ -45,16 +45,18 @@ sum(cutDownData$SA.x)
 
 # Caulculate the shrinkage c for the JS Estimator
 k <- nrow(cutDownData) # number of unknown means
-pbar <- 1 - mean(cutDownData$GA.x)/mean(cutDownData$SA.x) #total average of averages
-pbar
+pbar <- mean(cutDownData$SvP.x)#total average of averages
+n <- mean(cutDownData$SA.x) #average number of shots against
 phat <- cutDownData$SvP.x # Sample means, the MLEs
-c <- 1 - (k-3)*(phat*(1 - phat)/cutDownData$SA.x)/sum((phat - pbar)^2) # apply the shrinkage formula
+
+c <- 1 - (k-3)*(pbar*(1 - pbar)/n)/sum((phat - pbar)^2) # apply the shrinkage formula
 cAdjust <- mapply(min, c, 1)
 cAdjust <- mapply(max,c, 0)
 cAdjust
+
 # Calculate our MSE values for JS Estimator and the MLE (SvP.x)
 meanSq <- function(x, y){sqrt(mean((x-y)^2))}
-cutDownData$JS <- p.bar + cAdjust*(phat - pbar) # create a column for JS estimates
+cutDownData$JS <- pbar + cAdjust*(phat - pbar) # create a column for JS estimates
 cutDownData$SMMS <- mapply(meanSq,cutDownData$SvP.x, cutDownData$SvP.y)
 cutDownData$JSMS <- mapply(meanSq,cutDownData$JS, cutDownData$SvP.y)
 meanSq(cutDownData$SvP.x, cutDownData$SvP.y) ## total MLE MSE 
@@ -67,6 +69,12 @@ meanSq(pbar, cutDownData$SvP.y) ## total JS MSE
 ###       Visualizations         ###
 ### ---------------------------  ###
 
+# _________ #
+### TABLE ###
+# _________ #
+
+tableData <- subset(cutDownData, select=c("Player","SvP.x","SvP.y","JS", "SMMS", "JSMS"))
+
 
 ### Code for side-by-side bar plots of MSE for JS and MLE ###
 ### ----------------------------------------------------- ###
@@ -74,28 +82,39 @@ meanSq(pbar, cutDownData$SvP.y) ## total JS MSE
 cutDown <- subset(cutDownData, select=c("Player", "SMMS", "JSMS"))
 cutDown$Player <- sub(".*? (.+)", "\\1", cutDown$Player)
 cutDown.long<-melt(cutDown)
+avMLE <- mean(cutDown$SMMS)
+avJS <- mean(cutDown$JSMS)
 ggplot(cutDown.long,aes(Player,value,fill=variable))+
-  geom_bar(stat="identity",position="dodge") +
+  geom_point(stat="identity",position="dodge", aes(color = variable)) +
   theme(axis.text.x=element_text(angle = -55, hjust = 0)) +
- scale_fill_manual(values=c("#E69F00", "#56B4E9"))
-
-
+  labs(title = "Mean Squared Error of MLE vs JS", y = "Mean Squared Error") +
+  geom_hline(aes(yintercept = avMLE)) +
+  geom_hline(aes(yintercept = avJS))
+ 
+###     Code for Line-Plots inspired by Michael Lopez     ###
 ### ----------------------------------------------------- ###
 
 cutDownSP <- subset(cutDownData, select=c("Player", "SvP.x", "JS", "SvP.y"))
 cutDownSP$Player <- sub(".*? (.+)", "\\1", cutDownSP$Player)
-cutDownSP <- cutDownSP[sample(nrow(cutDownSP),10),]
+cutDownSP <- cutDownSP[sample(nrow(cutDownSP),12),]
 cutDownSP.long<-melt(cutDownSP)
 SPmelt <- melt(cutDownSP, id="Player")
 attach(SPmelt)
 new <- SPmelt[order(SPmelt$Player),]
 detach(SPmelt)
 
-ggplot(data=new, aes(x=variable, y=value)) + geom_line(aes(group=Player, color=Player), linetype="solid") + geom_point(aes(color=Player))
+ggplot(data=new, aes(x=variable, y=value)) +
+ geom_line(aes(group=Player, color=Player), linetype="solid") +
+ geom_point(aes(color=Player)) +
+ scale_x_discrete(labels=c("SvP.x" = expression(hat(p)['MLE']), "JS" = expression(hat(p)["JS"]),
+                            "SvP.y" = expression('p'["season"]))) + 
+  labs(title = "Visualization of Stein-Estimator", x = "Estimator", y = "Save Percentage")
+#--------------------------------------------#
+
 #ggplot(cutDownSP.long,aes(Player,value,fill=variable))+
  # geom_bar(stat="identity",position="dodge") +
   #theme(axis.text.x=element_text(angle = -55, hjust = 0)) +
-  c#oord_cartesian(ylim = c(.85, 1))
+  #coord_cartesian(ylim = c(.85, 1))
   #coord_flip() 
 
 #SPplot <- cutDownSP > gather(key, value, -Player)
