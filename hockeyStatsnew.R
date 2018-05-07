817bdaa4a8e12b9ba56ea50d31d6e34308eee682
traversgrayson <travers.parsonsgrayson@gmail.com>
2018-05-06 20:54:29 -0400

    Remove Old Files

--- hockeyStatsnew.R
+++ hockeyStatsnew.R
@@ -11,10 +11,12 @@
 ### Goal: To use James-Stein Estimators to provide a better estimation of 
 ### end-of-season save percentages than the MLE
 
-install.packages("dplyr")
-install.packages("tidyr")
-install.packages("ggplot2")
-install.packages("reshape2")
+# install.packages("xtable")
+# install.packages("dplyr")
+# install.packages("tidyr")
+# install.packages("ggplot2")
+# install.packages("reshape2")
+
 library(dplyr)
 library(tidyr)
 library(ggplot2)
@@ -38,7 +40,8 @@ seasonSplit <- subset(seasonStats, GP >= 50)
 
 # Merge the split and season data and remove unnecessary columns
 mergedData <- merge(subSplit,seasonSplit, by = "Player")
-cutDownData <- subset(mergedData, select=c("Player", "GA.x", "SA.x", "GA.y", "SA.y", "Sv..x", "Sv..y"))
+cutDownData <- subset(mergedData, select=c("Player", "GA.x", "SA.x", 
+                                           "GA.y", "SA.y", "Sv..x", "Sv..y", "GP.y"))
 cutDownData$SvP.x <- cutDownData$Sv..x/100
 cutDownData$SvP.y <- cutDownData$Sv..y/100
 sum(cutDownData$SA.x)
@@ -50,18 +53,18 @@ n <- mean(cutDownData$SA.x) #average number of shots against
 phat <- cutDownData$SvP.x # Sample means, the MLEs
 
 c <- 1 - (k-3)*(pbar*(1 - pbar)/n)/sum((phat - pbar)^2) # apply the shrinkage formula
-cAdjust <- mapply(min, c, 1)
-cAdjust <- mapply(max,c, 0)
-cAdjust
 
 # Calculate our MSE values for JS Estimator and the MLE (SvP.x)
 meanSq <- function(x, y){sqrt(mean((x-y)^2))}
-cutDownData$JS <- pbar + cAdjust*(phat - pbar) # create a column for JS estimates
+cutDownData$JS <- pbar + c*(phat - pbar) # create a column for JS estimates
 cutDownData$SMMS <- mapply(meanSq,cutDownData$SvP.x, cutDownData$SvP.y)
 cutDownData$JSMS <- mapply(meanSq,cutDownData$JS, cutDownData$SvP.y)
 meanSq(cutDownData$SvP.x, cutDownData$SvP.y) ## total MLE MSE 
 meanSq(cutDownData$JS, cutDownData$SvP.y)
 meanSq(pbar, cutDownData$SvP.y) ## total JS MSE
+
+mean(cutDownData$JSMS)
+mean(cutDownData$SMMS)
 
 ### ----------------------------------- ###
 
@@ -70,13 +73,15 @@ meanSq(pbar, cutDownData$SvP.y) ## total JS MSE
 ### ---------------------------  ###
 
 # _________ #
-### TABLE ###
+### TABLES ###
 # _________ #
+cutDownData$which <- cutDownData$JSMS <= cutDownData$SMMS
+library(xtable)
+tableData <- subset(cutDownData, select=c("Player","SvP.x","JS","SvP.y", "SMMS", "JSMS"))
+begData <- subset(cutDownData, select=c("Player","GA.x","SA.x","SvP.x","SvP.y", "GP.y" ))
 
-tableData <- subset(cutDownData, select=c("Player","SvP.x","SvP.y","JS", "SMMS", "JSMS"))
 
-
-### Code for side-by-side bar plots of MSE for JS and MLE ###
+### Code for Scatterplots of MSE for JS and MLE ###
 ### ----------------------------------------------------- ###
 
 cutDown <- subset(cutDownData, select=c("Player", "SMMS", "JSMS"))
@@ -96,7 +101,6 @@ ggplot(cutDown.long,aes(Player,value,fill=variable))+
 
 cutDownSP <- subset(cutDownData, select=c("Player", "SvP.x", "JS", "SvP.y"))
 cutDownSP$Player <- sub(".*? (.+)", "\\1", cutDownSP$Player)
-cutDownSP <- cutDownSP[sample(nrow(cutDownSP),12),]
 cutDownSP.long<-melt(cutDownSP)
 SPmelt <- melt(cutDownSP, id="Player")
 attach(SPmelt)
@@ -108,20 +112,8 @@ ggplot(data=new, aes(x=variable, y=value)) +
  geom_point(aes(color=Player)) +
  scale_x_discrete(labels=c("SvP.x" = expression(hat(p)['MLE']), "JS" = expression(hat(p)["JS"]),
                             "SvP.y" = expression('p'["season"]))) + 
-  labs(title = "Visualization of Stein-Estimator", x = "Estimator", y = "Save Percentage")
+  labs(title = "Visualization of Stein-Estimator", x = "Estimator", y = "Save Percentage") +
+  geom_hline(aes(yintercept = pbar))
 #--------------------------------------------#
 
-#ggplot(cutDownSP.long,aes(Player,value,fill=variable))+
- # geom_bar(stat="identity",position="dodge") +
-  #theme(axis.text.x=element_text(angle = -55, hjust = 0)) +
-  #coord_cartesian(ylim = c(.85, 1))
-  #coord_flip() 
 
-#SPplot <- cutDownSP > gather(key, value, -Player)
-#ggplot(SPplot, mapping = aes(x = Player, y = value, color = key)) + geom_line()
-
-# Grouped
-#ggplot(cutDownData,aes(x=Player)) + 
-# geom_bar(aes(y=SMMS),stat="identity",position="dodge",col="blue") +
-#  geom_bar(position="dodge") +   
-#geom_bar(aes(y=JSMS),stat="identity",position="dodge",col="yellow")
